fef872e42542857fc8f8c6a1a8512f91094e7bfc
GABRIEL SANTOS COUTINHO <gabriel.scoutinho4@senacsp.edu.br>
2024-11-07 20:44:43 -0300

    QRcode

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * Simples simulador de uma lampada
+ * @author Gabriel Coutinho
+ */
+
+function quebrar() {
+    document.getElementById('lamp').src="img/broken.jpg"
+    //reproduzindo um arquivo de audio no JS
+    //Passo 1: copiar o arqui de audio para o projeto
+    //Passo 2: Usar a classe audio (Biblioteca interna do JS)
+    let som = new Audio()
+    som.src = "sound/glassbreaking.wav"
+    som.play()
+}
