9f358938a4f9f5dd654d998590f2b6cee89cabdb
GABRIEL SANTOS COUTINHO <gabriel.scoutinho4@senacsp.edu.br>
2024-11-07 22:16:25 -0300

    controle

--- app.js
+++ app.js
@@ -3,7 +3,13 @@
  * @author Gabriel Coutinho
  */
 
+let chave = false
+let lampada = true
+
+
+ 
 function quebrar() {
+    if(lampada === true) {
     document.getElementById('lamp').src="img/broken.jpg"
     //reproduzindo um arquivo de audio no JS
     //Passo 1: copiar o arqui de audio para o projeto
@@ -11,8 +17,23 @@ function quebrar() {
     let som = new Audio()
     som.src = "sound/glassbreaking.wav"
     som.play()
+    // apoio alogica para o JS identificar a lampada quebrada
+    lampada = false 
+    }
 }
 
 function onoff() {
-    document.getElementById('interruptor').src="img/swon.png"
+    if (chave === false && lampada === true) {
+        //ligar a chave
+        document.getElementById('interruptor').src="img/swon.png"
+        chave = true // O JS agora sabe qie a chave está ligada
+         //acender a lampada
+         document.getElementById('lamp').src="img/on.jpg"
+    } else if (lampada === true){
+        document.getElementById('interruptor').src="img/swoff.png"
+        chave = false
+        //desligar lampada
+        document.getElementById('lamp').src="img/off.jpg"
+    }
+    
 }
